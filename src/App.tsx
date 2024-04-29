72022f0d0c03e69e9f5b5a3ad84cbd39dbdb4def
Joseph An <52297896+enyoukai@users.noreply.github.com>
2024-04-29 20:39:07 +0000

    setting up stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
 import { useState } from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
+import Home from './pages/Home'
+import Test from './pages/Test'
+import Results from './pages/Results'
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/test" element={<Page/>} />
-
+        <Route path="/test" element={<Test/>} />
+        <Route path="/results" element={<Results/>} />
       </Routes>
 
     </Router>
-  )
-}
-
-function Home()
-{
-  return (
-    <div>Home</div>
-  )
-}
-function Page()
-{
-  return (
-    <div className='w-screen h-screen bg-[#dcbdff]'>
-      <div className='flex flex-col items-center h-full p-10 gap-5'>
-        <div className="w-1/4 text-center text-gray-900 text-xl font-comfortaa font-bold">Q8/12: Suddenly, the robot glides in, it whispers something to the cats that makes them stop.</div>
-        <img src="https://via.placeholder.com/150" alt="robot" className="w-1/4 rounded-full" />
-        <button className="bg-gray-200 text-gray-900 p-2 rounded-sm">turn into a sigma</button>
-        <button className="bg-gray-200 text-gray-900 p-2 rounded-sm">turn into a sigma</button>
-      </div>
-    </div>
   )
 }
 
